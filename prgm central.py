458291ae158b75d391206d4d760dc7bc34bf88fa
Sayama3 <39456336+Sayama3@users.noreply.github.com>
2018-05-21 21:42:54 +0400

    Add files via upload

---
+++ prgm central.py
@@ -0,0 +1,16 @@
+from Iannis import *
+from Evan import *
+#from Michel import *
+from tkinter import *
+import time
+
+
+def main():
+    cube = creationCube()
+    snake = analyse(cube)
+
+    print(snake)
+    
+    
+if __name__ == "__main__":
+    main()
