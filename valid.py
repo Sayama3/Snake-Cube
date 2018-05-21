f11a932c8a3990e23778df98b901c4109c1decb7
Sayama3 <39456336+Sayama3@users.noreply.github.com>
2018-05-22 02:24:04 +0400

    Update valid.py

--- valid.py
+++ valid.py
@@ -1,4 +1,3 @@
-from math import *
 def valid(numeroCube,position,snake,cube):
 	if snake[position] != 0:
 		return -1
@@ -7,9 +6,10 @@ def valid(numeroCube,position,snake,cube):
 		coherence(numeroCube,position,snake,cubePositif)
 
 def positif(cube): #transformer le cube en valeur positif
+	import math
 	cubePositif = list()
 	for i in cube:
-		i = int( fabs(i) )
+		i = int( math.fabs(i) )
 		cubePositif.append(i)
 		return cubePositif
 
@@ -17,6 +17,8 @@ def coherence(numeroCube,position,snake,cubePositif):
 	test = 0 #variable de test afin de vérifié si la position est comprise dedans
 	numeroCube1 = numeroCube - 1 # variable de la position précédente
 	numeroCube2 = numeroCube - 2 # variable de la position précédente de la position précédente
+	cube1 = snake.index[numeroCube1]
+	cube2 = snake.index[numeroCube2]
 	
 	#variable de déplacement dans le plan
 	x = 1
