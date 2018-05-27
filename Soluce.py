f31d00a08bd10de1dd88214a0473332c2b80f656
grrrrrrrrrrr <39482827+grrrrrrrrrrr@users.noreply.github.com>
2018-05-27 12:01:44 +0400

    Add files via upload

--- Soluce.py
+++ Soluce.py
@@ -3,11 +3,13 @@ from Affichage import *
 from valid import *
 
 snake = constructionSnake()
-variablePourMichel = snake #Variable que Michel à besoin pour ses fonctions
+assert sum(snake) == 27 #vérifier que la liste comporte au total 27 cubes
+
 N = 5
 xdep = 1       # nombre de pièces à bouger dans l'axe x
 ydep = N       # nombre de pièces à bouger dans l'axe y
 zdep = N*N     # nombre de pièces à bouger dans l'axe z
+empty = 0
 
   # définition du cube comme un cube 5x5x5 avec des bords remplis mais un milieu vide pour faciliter la détection des bords
 haut = [-1]*N*N
@@ -21,26 +23,13 @@ def pos(x, y, z):
     """Convertir x,y et z en position dans la liste des cubes"""
     return x+y*ydep+z*zdep
 
-def Pcube(cube, margin=1):
-    """afficher le cube"""
-    for z in range(margin,N-margin):
-        for y in range(margin,N-margin):
-            for x in range(margin,N-margin):
-                v = cube[pos(x,y,z)]
-                if v == 0:
-                    s = " . "
-                else:
-                    s = "%02d " % v
-                print(s, sep="", end="")
-            print()
-        print()
-
 def place(cube, position, direction, longueur, Numpiece):
     """Placer un segment dans le cube"""
     Numpiece = int()
+    longueur = int()
     for _ in range(longueur):
         position += direction
-        assert cube[position] == 0
+        assert cube[position] == empty
         cube[position] = Numpiece
         Numpiece += 1
     return position
@@ -49,41 +38,42 @@ def unplace(cube, position, direction, longueur):
     """supprimer un segment du cube"""
     for _ in range(longueur):
         position += direction
-        cube[position] = 0
+        cube[position] = empty
 
-def moves(position, direction, cube, Numpiece, variablePourMichel):
+def moves(position, direction, cube, Numpiece, longueur):
     """renvoyer le mouvement valide pour la position actuelle"""
     Mvalides = []
     for Ndirections in directions:
         # Impossible de continuer dans la même direction ou l'inverse de la même direction
         if Ndirections == direction or Ndirections == -direction:
             continue
-        if valid(Numpiece, position, variablePourMichel, cube):
+        if valid(numeroCube, position, snake, cube):
             Mvalides.append(Ndirections)
     return Mvalides
 
-def solve(cube, position, direction, snake, Numpiece, variablePourMichel):
+def solve(cube, position, direction, snake, Numpiece):
     """le solveur de cube"""
-    if len(snake) == 0:
+    serpent = len(snake)
+    if len(snake) == serpent:
         print("Solution")
-        Pcube(cube)
+        print(visualisationCube(cube))
         return
     longueur, snake = snake[0], snake[1:]
-    Mvalides = moves(position, direction, cube, Numpiece, variablePourMichel)
+    Mvalides = moves(position, direction, cube, Numpiece, longueur)
     for Ndirections in Mvalides:
-        Nposition = place(position, Ndirections, longueur, Numpiece)
-        solve(cube, Nposition, Ndirections, snake, Numpiece+longueur, variablePourMichel)
+        Nposition = place(cube, position, Ndirections, longueur, Numpiece)
+        solve(cube, Nposition, Ndirections, snake, Numpiece+longueur)
         unplace(cube, position, Ndirections, longueur)
         
 
-def main(variablePourMichel, pos, xdep, snake,cube):
+def main(pos, xdep, snake, cube, place, solve):
     # La première pièce doit être placée le long d'un bord
     position = pos(0,1,1)
     direction = xdep
     longueur = snake[0]
     position = place(cube, position, direction, longueur, 1)
-    solve(cube, position, direction, snake[1:], longueur + 1, variablePourMichel)
+    solve(cube, position, direction, snake[1:], longueur + 1)
     
 
 if __name__ == "__main__":
-    main(variablePourMichel, pos, xdep, snake,cube)
+    main(pos, xdep, snake, cube, place, solve)
